1095b53a6c5d2c2958a524fd7335ecf3dbb57c00
liuzijian <liuzijian@hexindai.com>
2019-03-05 15:28:45 +0800

    万恶背包！！

--- src/main/java/com/evolution/leetcode/arr/StockCertifiTrade2.java
+++ src/main/java/com/evolution/leetcode/arr/StockCertifiTrade2.java
@@ -1,7 +1,7 @@
 package com.evolution.leetcode.arr;
 
 /**
- * @Description: 714. 买卖股票的最佳时机含手续费
+ * @Description: 714. 买卖股票的最佳时机含手续费 TODO
  *
  * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
  * 你可以无限次地完成交易，但是你每次交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
@@ -29,7 +29,60 @@ package com.evolution.leetcode.arr;
  */
 public class StockCertifiTrade2 {
 
+    /*public int maxProfit(int[] prices, int fee) {
+        int len = prices.length;
+        if(len<=1){
+            return 0;
+        }
+
+        int min=prices[0],max=prices[0],tempMin=min;
+        int income=0,tempIncome=0;
+        boolean add = false;
+        for(int i=1;i<len;i++){
+            if(prices[i]>=max){
+                max = prices[i];
+                int temp = max-tempMin-fee;
+                if(temp>tempIncome){
+                    tempIncome = temp;
+                    add = true;
+                    if(tempIncome+income<max-min-fee){
+                        income = max-min-fee;
+                        tempIncome = 0;
+                    }
+                }
+            }else{
+                if(add){
+                    income += tempIncome;
+                    tempIncome = 0;
+                }
+                tempMin = prices[i];
+                max = prices[i];
+            }
+
+            if(min>prices[i]){
+                min = prices[i];
+            }
+        }
+
+        income += tempIncome;
+
+        return income;
+    }*/
+
+    //  buy表示当天买入或不买入的最高利润，sell表示当天卖出或不卖出的最高利润
+    //  注意sell之前需要先buy，也就是sell的利润和buy的利润有关；同理buy之前需要sell，也就是buy后的利润和sell的利润有关
     public int maxProfit(int[] prices, int fee) {
+        int buy = -prices[0];
+        int sell = 0;
+        for(int i = 1;i<prices.length;i++){
+            sell = Math.max(sell,prices[i] + buy - fee);
+            buy = Math.max(buy,sell - prices[i]);
+        }
+        return sell;
+    }
+
+    //背包方式，相当于穷举，重复性大，时效低
+    /*public int maxProfit(int[] prices, int fee) {
         int len = prices.length;
         if(len<=1){
             return 0;
@@ -48,12 +101,13 @@ public class StockCertifiTrade2 {
             }
         }
         return max;
-    }
+    }*/
 
     public static void main(String[] args) {
         StockCertifiTrade2 trade2 = new StockCertifiTrade2();
-        int prices[] = {1,3,2,8,4,9};
-        int fee = 2;
+//        int prices[] = {1,3,2,8,4,9};
+        int prices[] = {1,3,7,5,10,3};
+        int fee = 3;
         int res = trade2.maxProfit(prices,fee);
         System.out.println(res);
     }
--- src/main/java/com/evolution/leetcode/str/WordsSplit2.java
+++ src/main/java/com/evolution/leetcode/str/WordsSplit2.java
@@ -1,6 +1,11 @@
 package com.evolution.leetcode.str;
 
+import org.assertj.core.util.Lists;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Description: 140-单词拆分II，难度升级    TODO
@@ -47,11 +52,63 @@ import java.util.List;
  */
 public class WordsSplit2 {
 
+    //"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    //["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+    public boolean canSplit(String s, Map<String,String> dictMap) {
+        int n = s.length();
+        boolean[] pack = new boolean[n+1];
+        pack[0] = true;
+        for(int i=1;i<=n;i++){
+            for(int k=0;k<i;k++){
+                if(pack[k] && dictMap.get(s.substring(k,i))!=null){
+                    pack[i] = true;
+                    break;
+                }
+            }
+        }
+        return pack[n];
+    }
+
     public List<String> wordBreak(String s, List<String> wordDict) {
-        return null;
+        Map<String,String> dictMap = wordDict.stream().collect(Collectors.toMap(x->{return x;}, v->{return "";}));
+        boolean canSplit = canSplit(s,dictMap);
+        if(!canSplit){
+            return Collections.emptyList();
+        }
+
+        int len = s.length();
+        LinkedList<String>[] m = new LinkedList[len+1];
+
+        for(int i=1;i<=len;i++){
+            LinkedList<String> iList = null;
+            for(int k=0;k<i;k++){
+                String temp = s.substring(k,i);
+                if(dictMap.get(temp)!=null){
+                    if(iList==null){
+                        iList = new LinkedList<>();
+                    }
+                    if(k==0){
+                        iList.add(temp);
+                    }else if(m[k]!=null && m[k].size()>0){
+                        LinkedList<String> beforeList = m[k];
+                        for(String before:beforeList){
+                            iList.add(before+" "+temp);
+                        }
+                    }
+                }
+            }
+            m[i] = iList;
+        }
+        return m[len];
     }
 
     public static void main(String[] args) {
-
+        String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        List<String> wordDict = Lists.newArrayList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa");
+        WordsSplit2 ws2 = new WordsSplit2();
+        List<String> resList = ws2.wordBreak(s,wordDict);
+        System.out.println(resList);
     }
 }
